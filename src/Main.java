ea4c6f16dacacef9a670414a9f7aebce24bf3aa3
Hong27 <2jh627@naver.com>
2023-03-19 19:58:07 +0900

    리팩토링

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,8 @@
+import controller.MainController;
+
 public class Main {
     public static void main(String[] args) {
-        int selectedMenu = 0;
-        MySQL_JDBC mySQL = new MySQL_JDBC();
-        Menu mainMenu = new Menu();
-        AddData add_Data = new AddData(mySQL);
-        FindBy find= new FindBy(mySQL);
-
-        while (true) {
-            selectedMenu = mainMenu.choiceMenu();
-            if (selectedMenu == 0) {
-                break;
-            }
-            switch (selectedMenu) {
-                case 1:
-                    add_Data.addFromFile();
-                    break;
-                case 2:
-                    find.findByKeyword("Title");
-                    break;
-                case 3:
-                    find.findByKeyword("Totalnum");
-                    break;
-                case 4:
-                    find.findByKeyword("Releasedate");
-                    break;
-                default:
-                    break;
-            }
-        }
-        System.out.println("프로그램을 정상적으로 종료합니다.");
+        MainController mc = MainController.getInstance();
+        mc.run();
     }
 }
